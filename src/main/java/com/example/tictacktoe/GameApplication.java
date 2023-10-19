b8508fc9b3046d147bdff0d6360b23faceaf16bc
august <august.rydnell@gmail.com>
2023-10-19 12:26:02 +0200

    created buttons and fixed positioning

--- src/main/java/com/example/tictacktoe/GameApplication.java
+++ src/main/java/com/example/tictacktoe/GameApplication.java
@@ -11,7 +11,7 @@ public class GameApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(GameApplication.class.getResource("game-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 340, 500);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
